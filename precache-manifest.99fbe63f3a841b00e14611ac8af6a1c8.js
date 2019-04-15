9dd90da486f97f4872c3b30dd475de5ff418901f
Ricardo Baeza <baezaric3600@gmail.com>
2019-04-15 03:14:39 -0600

    Updates

---
+++ precache-manifest.99fbe63f3a841b00e14611ac8af6a1c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffa85474708cc07ab1d2",
+    "url": "/E-Commerce-App/static/css/main.ce6f325a.chunk.css"
+  },
+  {
+    "revision": "ffa85474708cc07ab1d2",
+    "url": "/E-Commerce-App/static/js/main.685b0812.chunk.js"
+  },
+  {
+    "revision": "db2a39d328836cbb23c4",
+    "url": "/E-Commerce-App/static/js/runtime~main.43e983b8.js"
+  },
+  {
+    "revision": "4598b4b1c02153821f4c",
+    "url": "/E-Commerce-App/static/css/2.72ad0b78.chunk.css"
+  },
+  {
+    "revision": "4598b4b1c02153821f4c",
+    "url": "/E-Commerce-App/static/js/2.f69eca9f.chunk.js"
+  },
+  {
+    "revision": "c770b9d0a235e48901abd88af7c55a71",
+    "url": "/E-Commerce-App/index.html"
+  }
+];
