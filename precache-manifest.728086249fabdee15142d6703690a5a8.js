f364ad9ba390ba4f4bc58d6fe5ee15787c2a2039
Ricardo Baeza <baezaric3600@gmail.com>
2019-04-15 08:57:34 -0600

    Updates

---
+++ precache-manifest.728086249fabdee15142d6703690a5a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fecf6906ad56aaeb8745",
+    "url": "/E-Commerce-App/static/css/main.4beab487.chunk.css"
+  },
+  {
+    "revision": "fecf6906ad56aaeb8745",
+    "url": "/E-Commerce-App/static/js/main.3c062968.chunk.js"
+  },
+  {
+    "revision": "db2a39d328836cbb23c4",
+    "url": "/E-Commerce-App/static/js/runtime~main.43e983b8.js"
+  },
+  {
+    "revision": "4598b4b1c02153821f4c",
+    "url": "/E-Commerce-App/static/css/2.72ad0b78.chunk.css"
+  },
+  {
+    "revision": "4598b4b1c02153821f4c",
+    "url": "/E-Commerce-App/static/js/2.f69eca9f.chunk.js"
+  },
+  {
+    "revision": "5baf2e40d20e3e04344caee3b9e27c34",
+    "url": "/E-Commerce-App/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fb2af9a47a584244669ee1c47c0ccd6e.js"
+  "/E-Commerce-App/precache-manifest.728086249fabdee15142d6703690a5a8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/E-Commerce-App/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
